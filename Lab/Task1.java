21b51eb36e90d6cf97f70d4993551bd2c30e00b3
Saifyousuf <saifhasan546em@gmail.com>
2025-10-29 10:16:12 +0600

    Import Scanner and read student score input
    Added Scanner import and initialized Scanner instance.

--- Lab/Task1.java
+++ Lab/Task1.java
@@ -1,5 +1,11 @@
 package Lab;
-
+import java.util.Scanner;
 public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner();
+        System.out.println("enter student1 score=45");
+        double mark1 = scanner.nextDouble();
+
+    }
     
 }
